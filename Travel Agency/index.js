86959ecc3061cc9776ce31fc1d55bf8dd932e4c3
Theyusuf41 <yusufakrglu@gmail.com>
2022-05-30 14:29:17 +0200

    Last Chances
    Footer, Gallery and destination have been made.

--- Travel Agency/index.js
+++ Travel Agency/index.js
@@ -3,3 +3,29 @@ let navbar = document.querySelector('.header .navbar');
 document.querySelector('#menu-btn').onclick = () => {
   navbar.classList.toggle('active');
 }
+
+
+document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn =>{
+  btn.onclick = () =>{
+    let src = btn.getAttribute('data-src');
+    document.querySelector('.about .video-container .video').src = src
+  }
+})
+
+/*  Masonry kodlari
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 500
+});
+
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.grid', {
+  // options
+  percentPosition: true
+});
+*/
+const date = document.querySelector('.date');
+date.innerHTML = new Date().getFullYear();
